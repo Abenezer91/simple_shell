8092affb1cad5b0e709ce29d00ef127aeed301c7
Abenezer91 <alxabenezer@gmail.com>
2023-05-24 07:18:41 -0700

    File as input

---
+++ realloc.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+
+/**
+ * _memset - input
+ * @s: input
+ * @b: input
+ * @n: input
+ * Return: return s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - input
+ * @pp: input
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - input
+ * @ptr: input
+ * @old_size: input
+ * @new_size: input
+ *
+ * Return: return 0
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
+
